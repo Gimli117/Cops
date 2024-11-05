3d25df9268e7df914024e044fba4a6fb1c4da051
Itsmem99 <maha41989@gmail.com>
2024-11-05 14:04:39 +0100

    ligga till sammanfattningar

--- City.cs
+++ City.cs
@@ -9,12 +9,40 @@ namespace TjuvPolis
 {
     internal class City
     {
+        /// <summary>
+        /// citySize är en fält som är medlem i city som kan inte kommas åt i andra classer för att den är privat 
+        /// även det är typ av AreaSize 
+        /// </summary>
+      
         private readonly AreaSize citySize;
+        /// <summary>
+        /// perisonsize är en fält som är medlem i city som kan inte kommas åt i andra classer för att den är privat
+        /// även det är typ av AreaSize
+        /// </summary>
         private readonly AreaSize prisonSize;
+        /// <summary>
+        /// poorPlaceSize är en fält som är medlem i ciy som kan inte kommas åt i andra classer för att den är privat 
+        /// även det är typ av Areasize .
+        /// </summary>
+       
         private readonly AreaSize poorPlaceSize;
+        /// <summary>
+        /// Wall är en char som är en fält som är medlem i city som kan inte kommas åt i an dra classer för att den är privat
+        /// även det är en konstant altså man kan inte ändra det som virable
+        /// </summary>
         const char wall = '█';
+        
+        /// <summary>
+        /// _population är en medlem i city classen som är special fält som heter backingfält för propeprty Population
+        ///  och där kan skickas ut genom get metoden och värdet ändras i den genom sett metoden
+        /// </summary>
 
         private List<Person> _population;
+        /// <summary>
+        /// Population är en medlem i city classen som är proppety som kan använda en fält som heter backningsfält
+        /// även propety kan innehålla get metoden och värdet ändras genom sett metoden . Det är ett Public proetty
+        // som har accecee modefire, denna fältet kan användas i andra classer.
+        /// </summary>
         public List<Person> Population { 
             get
             {
@@ -26,10 +54,17 @@ namespace TjuvPolis
             }
         }      // Lista med alla personer i staden
 
+        /// <summary>
+        /// City är en Public som är kunstroktur vilken kan skapa obejkt i andra classer och den skrivs efter ordet new
+        /// </summary>
         public City()
         {
-            _population = new List<Person>();
+            //har vi skapat en lista med hjälp av kunstrokter vilken är kan skrivas efter ord new , vi har också skapat en lista
+            //av typen peron i city classen då vi lagt till en ny lista till _population.
 
+            _population = new List<Person>();
+            //har vi kallat en kunstrokter som är typ av AreaSize i city classen 
+            //
             citySize = new AreaSize(0, 0, 100, 25);
             prisonSize = new AreaSize(105, 0, 130, 10);
             poorPlaceSize = new AreaSize(105, 15, 130, 25);
@@ -68,15 +103,14 @@ namespace TjuvPolis
 
                 CheckEncounters();
 
-                //Thread.Sleep(500);
-                Console.ReadLine();
+                Thread.Sleep(500);
+                //Console.ReadLine();
                 Console.Clear();
             }
         }
 
         public void DrawWalls(int MinWidthX, int MinHeightY, int MaxWidthX, int MaxHeightY, char? otherWall)
         {
-            Console.ForegroundColor = ConsoleColor.White;
             for (int x = MinWidthX; x <= MaxWidthX; x++)
             {
                 for (int y = MinHeightY; y <= MaxHeightY; y++)
@@ -93,26 +127,6 @@ namespace TjuvPolis
                     }
                 }
             }
-
-            Console.CursorLeft = 48;
-            Console.CursorTop = 26;
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("City");
-
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("\n\n Reports");
-            Console.CursorLeft = 0;
-            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
-
-            Console.CursorLeft = 115;
-            Console.CursorTop = 11;
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("Prison");
-
-            Console.CursorLeft = 113;
-            Console.CursorTop = 26;
-            Console.ForegroundColor= ConsoleColor.DarkGreen;
-            Console.WriteLine("Poor House");
         }
 
         public void DrawCity()
@@ -142,22 +156,21 @@ namespace TjuvPolis
                 else if (person is Police) ((Police)person).Scan(_population);
                 else if (person is Citizen) ((Citizen)person).GiveUp();
             }
-            Logger.PrintQueue();
         }
 
         public void CreatePopulation()
         {
-            for (int p = 0; p < 15; p++)        //Skapar 10 poliser och ger dem namn
+            for (int p = 0; p < 10; p++)        //Skapar 10 poliser och ger dem namn
             {
                 _population.Add(new Police($"P{p + 1}"));
             }
             
-            for (int t = 0; t < 15; t++)        //Skapar 30 tjuvar
+            for (int t = 0; t < 30; t++)        //Skapar 30 tjuvar
             {
                 _population.Add(new Thief($"T{t + 1}"));
             }
 
-            for (int c = 0; c < 20; c++)        //Skapar 40 medborgare
+            for (int c = 0; c < 40; c++)        //Skapar 40 medborgare
             {
                 _population.Add(new Citizen($"C{c + 1}"));
             }
