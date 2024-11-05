e466517dee70a73a5bafdf01e3ac2fd623b17f30
nightooi <rafaellost2021@outlook.com>
2024-11-05 14:32:36 +0100

    Revert "revert 6177537556a7897531f5e8a739f0091f5ff84ab6"
    This reverts commit e29027f80efb068f13660d42fb09a7bd0f243ed8.

--- Program.cs
+++ Program.cs
@@ -6,9 +6,8 @@ namespace TjuvPolis
     {
         static void Main()
         {
-            Console.OutputEncoding = Encoding.Latin1;
+            Console.OutputEncoding = Encoding.Unicode;
             City stad = new City();
-
             stad.DrawOutput();
         }
     }
